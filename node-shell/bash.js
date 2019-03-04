feba4fd752955b22ccdbb58b321585500b1ee891
Brandon Rowe <brandonhrowe@gmail.com>
2019-03-04 12:39:47 -0500

    process

--- node-shell/bash.js
+++ node-shell/bash.js
@@ -1,1 +1,9 @@
-console.log('test')
+ProcessingInstruction.stdout.write('prompt > ');
+
+ProcessingInstruction.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  ProcessingInstruction.stdout.write('You typed: ' + cmd);
+  ProcessingInstruction.stdout.write('\nprompt > ');
+
+})
